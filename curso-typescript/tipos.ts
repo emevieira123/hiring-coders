3679f6c9eecd47806442d9ec7bd84aeab3ae4c33
Emerson Vieira <emevieira@gmail.com>
2021-07-12 17:19:30 -0400

    typescript - tipos basiscos e never

--- curso-typescript/tipos.ts
+++ curso-typescript/tipos.ts
@@ -47,3 +47,16 @@ criar({
   propriedade: 1,
 })
 //criar('Emerson') //Dá erro
+
+//Never
+function loopInfinito(): never {
+  while (true) { }
+}
+
+function erro(mensagem: string): never {
+  throw new Error(mensagem);
+}
+
+function falha() {
+  return erro('Algo falhou');
+}
