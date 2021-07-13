b66b67025b7cb7501dbeb795aea02dd45170f7cb
Emerson Vieira <emevieira@gmail.com>
2021-07-13 12:19:43 -0400

    Utilizando modificadores de acesso

--- curso-typescript/classes.ts
+++ curso-typescript/classes.ts
@@ -14,3 +14,30 @@ const data = new Data(1, 1, 2021);
 console.log(data.dia);
 
 const data2 = new Data(1, 1);
+
+
+class Carro {
+  private velocidadeAtual: number = 0;
+
+  constructor(
+    public marca: string,
+    public modelo: string,
+    private velocidadeMaxima: number = 220
+  ) { }
+
+  private alterarVelocidade(delta: number) {
+    // FAÇO AS VALIDAÇÕES DE ACELERACAO E FRENAGEM
+
+  }
+
+  acelerar() {
+    this.alterarVelocidade(5);
+  }
+
+  frear() {
+    this.alterarVelocidade(-5);
+  }
+}
+
+const carro = new Carro('Chevrolet', 'Prisma', 250);
+carro.acelerar();
